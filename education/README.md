812e2dc1bbfbacf4ecee3e4fc706a6c9c63c743d
dinh-letat <dinhprococoa@gmail.com>
2024-09-11 21:10:20 +0700

    init project: education project

---
+++ education/README.md
@@ -0,0 +1,7 @@
+# EDUCATION - REACT APP #
+
+## Step by step to create react app ##
+
+1. Add tailwindcss
+2. Add google font
+3. 
---
+++ education/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span>
+        <h2 className="font-poppins text-gray-50 bg-black">Test</h2>
+        <h2 className="font-roboto">Test</h2>
+        <h2 className="font-lora">Test</h2>
+        <h2 className="font-serif">Test</h2>
+      </span>
+    </div>
+  );
+}
+
+export default App;
